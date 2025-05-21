46446db11eca5f375cf18f76cce3c9e8c5e7f21a
zenithfootball <zenithfootballleague@gmail.com>
2025-05-22 03:42:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -81,5 +81,6 @@ def logout():
     session.pop("admin", None)
     return redirect(url_for("index"))
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
